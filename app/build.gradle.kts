294041809fc07353381b4bcf5302360c30007ed1
ZidanAli909 <ahmadzidanali909@gmail.com>
2024-11-11 15:55:32 +0700

    - Menambah activity_diary_create - Menambah activity_diary_detail - dan lain-lain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.annotation)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/zidan/zidanapp/MainActivity.kt
+++ app/src/main/java/com/zidan/zidanapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.zidan.zidanapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,14 +24,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         initViews()
     }
 
     private fun initViews() {
+        // Bottom navigation
         with (binding) {
             val navHostFragment = supportFragmentManager.findFragmentById(MainFCV.id) as NavHostFragment
             MainBNV.setupWithNavController(navHostFragment.navController)
         }
+    }
+
+    // Options toolbar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //TODO: Interact menu items
+        return super.onOptionsItemSelected(item)
     }
 }
